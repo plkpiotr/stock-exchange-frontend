d40ef9c693f4a12bde7d7d7c8e95681f8b8ab0db
plkpiotr <plk.piotr.pp@gmail.com>
2019-09-04 02:28:59 +0200

    Change URL

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -1,7 +1,7 @@
-// https://stock-exchange-backend.herokuapp.com
+// http://localhost:8080
 
 const URLs = {
-  stockExchange: 'http://localhost:8080',
+  stockExchange: 'https://stock-exchange-backend.herokuapp.com',
   quandl: 'https://www.quandl.com/api/v3/datasets/WSE',
   key: '&api_key=MqbK19YZ9jDT8asyvzN-',
 };
